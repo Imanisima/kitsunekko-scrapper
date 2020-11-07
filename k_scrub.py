0e9b2ef0ba2e922177cf2d92faf341bd2af15a2c
Imani <imanicaza@gmail.com>
2020-11-07 12:10:14 -0600

    Updating decompression of files

--- k_scrub.py
+++ k_scrub.py
@@ -9,6 +9,8 @@ if sys.argv[1] = "" or None:
   print("Ex: k_scrub.py path/to/folder")
   print("Terminating program...")
   sys.exit()
+  
+trans_path = sys.argv[1]
 
 domain = "https://kitsunekko.net"
 sub_query = "/dirlist.php?dir=subtitles"
@@ -26,25 +28,88 @@ anime_list = find_anime_list(trans_elem)
 find_zip_files(anime_list, sys.argv[1])
 
 
-'''Get path to zip files for downloads.'''
-def find_zip_files(anime_list, trans_path)
+'''
+Get path to zip files for downloads.
+'''
 
-  for zip_link in anime_list:
-    zip_url = domain + anime_list[zip_link]
-    zip_res = requests.get(zip_url)
+print("Downloading from Kistunekko.net...")
 
-    soup = BeautifulSoup(zip_res.content, 'html.parser')
+for zip_link in anime_list:
+  zip_title = zip_link
+  zip_url = domain + anime_list[zip_link]
+  zip_res = requests.get(zip_url)
 
-    table_res = soup.find(id='flisttable')
-    trans_elem = table_res.find_all('a', class_='')
+  soup = BeautifulSoup(zip_res.content, 'html.parser')
 
-    for a_tag in trans_elem:
-      title_elem = a_tag.find('strong', class_='')
-      trans_title = clean_html(str(title_elem))
+  table_res = soup.find(id='flisttable')
+  trans_elem = table_res.find_all('a', class_='')
 
-      download_url = domain + "/" + a_tag["href"]
+  for a_tag in trans_elem:
+    trans_title = clean_html(str(zip_title))
+    download_url = domain + "/" + a_tag["href"]
+    download_files(download_url, trans_path + trans_title)
 
-      download_files(download_url, trans_path, anime_list[zip_link])
+print("Download complete.")
+
+dir_list = return_path_list(trans_path)
+
+print("Decompressing files...")
+decompress_files(dir_list, trans_path)
+print("Done!")
+
+
+'''Check if directory exists. If not, create one. '''
+def check_path(file_path):
+  if not os.path.exists(file_path):
+    print(f"creating dir: {file_path}")
+    os.mkdir(file_path)
+    
+def getListOfFiles(dir_path):
+
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dir_path)
+    allFiles = list()
+
+    # Iterate over all the entries
+    for entry in listOfFile:
+
+        # Create full path
+        fullPath = os.path.join(dir_path, entry)
+
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+  
+'''extract files from zip, rar, and .7zip files'''
+def decompress_files(trans_folder, trans_path):
+
+  for zip_file in trans_folder:
+    if ((".rar" in zip_file) or (".zip" in zip_file) or (".7z" in zip_file)):
+      save_to = os.path.splitext(zip_file)[0]
+      check_path(save_to)
+
+      with open(zip_file, "rb") as f:
+        try:
+          Archive(zip_file).extractall(save_to)
+          os.remove(zip_file) # remove zip file
+
+        except: # in case of a bad zip file or magic number error
+          pass
+        
+''' Gets a list of directories and subdirectories of the give path'''
+def return_path_list(file_path):
+  path_list = getListOfFiles(file_path)
+  path_list = list()
+
+  for (dirpath, dirnames, filenames) in os.walk(file_path):
+      path_list += [os.path.join(dirpath, file) for file in filenames]
+
+  return path_list
 
 '''
 Download file from kisunekko
@@ -52,17 +117,18 @@ Download file from kisunekko
 Dir: where to store the download
 URL: link to download the transcripts
 '''
-def download_files(url, dir, title):
-  dir = os.path.expanduser(dir)
-  if not os.path.exists(dir):
-      os.makedirs(dir)
+def download_files(url, dir):
+  zip_path = os.path.expanduser(dir)
+  download_to = zip_path + "/"
 
-  print("\nDownloading kitsunekko transcripts...")
+  
+  if not os.path.exists(zip_path):
+      os.makedirs(zip_path)
+      try:
+        wget.download(url=url, out=download_to)
 
-  if os.path.exists(os.path.join(dir, title)):
-      print(file, "already downloaded")
-  else:
-      wget.download(url=url, out=dir) 
+      except:
+        pass
 
   print("Downloads finished!")
 
